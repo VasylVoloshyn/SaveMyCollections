910bda29dc62f7ff138143a0839eabd8892407cc
Vasyl Voloshyn <voloshyn.v.v@gmail.com>
2022-07-04 15:38:53 +0300

    fix index

--- MyCollection/Pages/Bones/Details.cshtml.cs
+++ MyCollection/Pages/Bones/Details.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using MyCollection.Data;
 using MyCollection.Models;
+using MyCollection.Service;
 
 namespace MyCollection.Pages.Bones
 {
@@ -28,7 +29,15 @@ namespace MyCollection.Pages.Bones
                 return NotFound();
             }
 
-            var bone = await _context.Bones.FirstOrDefaultAsync(m => m.Id == id);
+            var bone = await _context.Bones
+                .Include(b=>b.BonePhotos)
+                .ThenInclude(b=>b.Photo)
+                .Include(b=>b.Signature)
+                .ThenInclude(b=>b.Person)
+                .Include(b=>b.Grade)
+                .Include(b=>b.Currency)
+                .ThenInclude(b=>b.Country)               
+                .FirstOrDefaultAsync(m => m.Id == id);
             if (bone == null)
             {
                 return NotFound();
@@ -36,6 +45,10 @@ namespace MyCollection.Pages.Bones
             else 
             {
                 Bone = bone;
+                foreach (var photo in Bone.BonePhotos.Select(b => b.Photo))
+                {
+                    photo.ImageUrl = ImageService.GetImageUrl(photo.ImageData);
+                }
             }
             return Page();
         }
---
+++ MyCollection/Service/MyCollectionService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyCollection.Service
+{
+    public class MyCollectionService
+    {
+        public static IList<CollectionItemInfo> GetCollectionItems()
+        {
+            List<CollectionItemInfo> items = new List<CollectionItemInfo>();
+
+            var bones = new CollectionItemInfo
+            {
+                MyColectionType = MyColectionType.Bone,
+                ImageUrl = "../Images/Bones.jpg",
+                CollectionUrl = "/Bones/Index"
+            };
+            var coins =  new CollectionItemInfo
+            {
+                MyColectionType = MyColectionType.Coin,
+                ImageUrl = "../Images/Coins.jpg",
+                CollectionUrl = "/Coins/Index"
+            };
+            var stamps = new CollectionItemInfo
+            {
+                MyColectionType = MyColectionType.Stamp,
+                ImageUrl = "../Images/Stamps.jpg",
+                CollectionUrl = "/Stamps/Index"
+            };
+
+            items.Add(bones);
+            items.Add(coins);
+            items.Add(stamps);
+            return items;            
+
+        }
+
+        
+        public enum MyColectionType
+        {
+            Bone,
+            Coin,
+            Stamp
+        }
+
+        public class CollectionItemInfo
+        {
+            public MyColectionType MyColectionType { get; set; }
+            public string ImageUrl { get; set; }
+
+            public string CollectionUrl { get; set; }
+
+        }
+            
+    }
+}
