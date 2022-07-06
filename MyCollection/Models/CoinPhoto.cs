81ad8e6a3436c1020a11fa1a5012a95d20221fa8
Vasyl Voloshyn <voloshyn.v.v@gmail.com>
2022-07-06 14:27:04 +0300

    change edit coins

--- MyCollection/Models/CoinPhoto.cs
+++ MyCollection/Models/CoinPhoto.cs
@@ -9,6 +9,8 @@ namespace MyCollection.Models
         [Required]
         public int CoinId { get; set; }
         public Coin Coin { get; set; } = null!;
+        public bool IsAvers { get; set; }
+        public bool IsRevers { get; set; }
         [Required]
         public int PhotoId { get; set; }
         public Photo Photo { get; set; } = null!;
--- MyCollection/Pages/Coins/Edit.cshtml.cs
+++ MyCollection/Pages/Coins/Edit.cshtml.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MyCollection.Data;
 using MyCollection.Models;
+using MyCollection.Service;
 
 namespace MyCollection.Pages.Coins
 {
@@ -29,26 +30,82 @@ namespace MyCollection.Pages.Coins
             {
                 return NotFound();
             }
-
-            var coin =  await _context.Coins.FirstOrDefaultAsync(m => m.Id == id);
+            var coin = await _context.Coins.Include(b => b.CoinPhotos).ThenInclude(b => b.Photo).FirstOrDefaultAsync(m => m.Id == id);
             if (coin == null)
             {
                 return NotFound();
             }
             Coin = coin;
-           ViewData["CoinGradeId"] = new SelectList(_context.CoinGrades, "Id", "Code");
+            foreach (var photo in Coin.CoinPhotos.Select(b => b.Photo))
+            {
+                photo.PreviewImageUrl = ImageService.GetImageUrl(photo.PreviewImageData);
+            }
+
+            ViewData["CoinGradeId"] = new SelectList(_context.CoinGrades, "Id", "Code");
            ViewData["DimeId"] = new SelectList(_context.Dimes, "Id", "Code");
             return Page();
         }
 
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(int? id, IFormFile? aversImage, IFormFile? reversImage)
         {
             if (!ModelState.IsValid)
             {
                 return Page();
             }
+
+            _context.Attach(Coin).State = EntityState.Modified;
+
+            var coinToUpdate = await _context.Coins.Include(b => b.CoinPhotos)
+                .ThenInclude(b => b.Photo).FirstOrDefaultAsync(b => b.Id == id);
+
+            if (coinToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            Coin.CoinPhotos = coinToUpdate.CoinPhotos;
+            bool isAversExist = Coin.CoinPhotos.Any(c=>c.IsAvers);
+            bool isReversExist = Coin.CoinPhotos.Any(c => c.IsRevers);
+            
+            List<Photo> photoToRemove = new List<Photo>();
+            if (aversImage != null)
+            {
+                if (isAversExist)
+                {
+                    photoToRemove.Add(Coin.CoinPhotos.Where(c=>c.IsAvers).First().Photo);
+                    Coin.CoinPhotos.Where(c=>c.IsAvers).First().Photo = await ImageService.CreateImageAsync(aversImage);
+                }
+                else
+                {
+                    var avers = new CoinPhoto();
+                    avers.CoinId = Coin.Id;
+                    avers.Photo = await ImageService.CreateImageAsync(aversImage);
+                    avers.IsAvers = true;
+                    Coin.CoinPhotos.Add(avers);
+                }
+            }
+
+            if (reversImage != null)
+            {
+                if (isReversExist)
+                {
+                    photoToRemove.Add(Coin.CoinPhotos.Where(c=>c.IsRevers).First().Photo);
+                    Coin.CoinPhotos.Where(c=>c.IsRevers).First().Photo = await ImageService.CreateImageAsync(reversImage);
+                }
+                else
+                {
+                    var revers = new CoinPhoto();
+                    revers.CoinId = Coin.Id;
+                    revers.Photo = await ImageService.CreateImageAsync(reversImage);
+                    revers.IsRevers = true;
+                    Coin.CoinPhotos.Add(revers);
+                }
+            }
+
+            _context.Photos.RemoveRange(photoToRemove);
+            //_context.Attach(Bone).State = EntityState.Modified;
 
             _context.Attach(Coin).State = EntityState.Modified;
 
