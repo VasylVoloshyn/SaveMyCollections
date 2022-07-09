bb89fb59628d2c9309cb521e4552529a84922d9e
Vasyl Voloshyn <voloshyn.v.v@gmail.com>
2022-07-09 13:51:52 +0300

    fix init database

---
+++ MyCollection/Data/DbInitializer.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyCollection.Models;
+
+namespace MyCollection.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(MyCollectionContext context)
+        {
+            context.Database.Migrate();
+            if (context.BoneGrades.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            
+            Grade crade = new Grade
+            {
+                Code = "UNC",
+                Name = "Uncirculated",
+                Description = "test"
+            };
+            context.BoneGrades.Add(crade);
+            context.SaveChanges();
+
+        }
+    }
+}
--- MyCollection/Program.cs
+++ MyCollection/Program.cs
@@ -1,30 +1,46 @@
-using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
 using MyCollection.Data;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddDbContext<MyCollectionContext>(o => o.UseSqlServer(
-    builder.Configuration.GetConnectionString("MyCollection")));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace MyCollection
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfNotExists(host);
+
+            host.Run();
+        }
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<MyCollectionContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
