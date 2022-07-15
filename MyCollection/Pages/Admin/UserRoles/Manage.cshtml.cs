c5a8ed4473ab3e97c989ec34ac3b635b10a666d9
Vasyl Voloshyn <voloshyn.v.v@gmail.com>
2022-07-15 15:36:43 +0300

    fix manage roles

---
+++ MyCollection/Pages/Admin/UserRoles/Manage.cshtml.cs
@@ -0,0 +1,90 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyCollection.Models;
+
+namespace MyCollection.Pages.Admin.UserRoles
+{
+    public class ManageModel : PageModel
+    {
+        private readonly MyCollection.Data.MyCollectionContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public ManageModel(MyCollection.Data.MyCollectionContext context,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _context = context;
+            _roleManager = roleManager;
+        }
+        [BindProperty]
+        public List<ManageUserRoles> ManageUserRoles { get; set; } = default!;
+        public async Task<IActionResult> OnGetAsync(Guid id)
+        {
+            ManageUserRoles = new List<ManageUserRoles>();
+            var userId = id.ToString();
+            ViewData["userId"] = userId;
+            
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                ViewData["ErrorMessage"] = $"User with Id = {userId} cannot be found";
+                return RedirectToPage("/NotFound");
+            }
+            ViewData["UserName"] = user.UserName;
+
+            foreach (var role in _roleManager.Roles.ToList())
+            {
+                var userRolesViewModel = new ManageUserRoles
+                {
+                    RoleId = role.Id,
+                    RoleName = role.Name
+                };
+                if (await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                    userRolesViewModel.Selected = true;
+                }
+                else
+                {
+                    userRolesViewModel.Selected = false;
+                }
+                ManageUserRoles.Add(userRolesViewModel);
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            var userId = id.ToString();
+
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return Page();
+            }
+            var roles = await _userManager.GetRolesAsync(user);
+            var result = await _userManager.RemoveFromRolesAsync(user, roles);
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Cannot remove user existing roles");
+                return Page();
+            }
+            result = await _userManager.AddToRolesAsync(user, ManageUserRoles.Where(x => x.Selected).Select(y => y.RoleName));
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Cannot add selected roles to user");
+                return Page();
+            }
+
+
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
